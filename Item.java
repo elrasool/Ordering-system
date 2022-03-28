6dba424234b79463ddf0c338b09c50cca1b97773
AboOmar <elrasool2@gmail.com>
2022-03-28 09:54:56 +0200

    After creating Item class

---
+++ Item.java
@@ -0,0 +1,18 @@
+class Item {
+    private float shippingWeight;
+    private String description;
+
+    public void getPriceForQuantity() {
+
+    }
+
+    public void getTax(){
+
+    }
+
+    public void inStock(){
+
+    }
+}
+
+
