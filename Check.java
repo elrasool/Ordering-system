df967b52b030a216596735ce322dc668a79bf8b9
AboOmar <elrasool2@gmail.com>
2022-03-28 10:08:16 +0200

    After creating Check class

---
+++ Check.java
@@ -0,0 +1,26 @@
+public class Check extends Payment{
+
+    private String name;
+    private String bankId;
+
+    public Check(String name, String bankId) {
+        this.name = name;
+        this.bankId = bankId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(String bankId) {
+        this.bankId = bankId;
+    }
+}
